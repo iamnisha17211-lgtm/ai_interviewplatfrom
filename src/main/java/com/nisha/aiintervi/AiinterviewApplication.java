8e22979c453b231fac3e057467860f273ec25984
ADMIN <ADMIN@DESKTOP-M2VA9L8>
2026-03-17 16:41:45 +0530

    initial commit - AI Interview Project

--- src/main/java/com/nisha/aiintervi/AiinterviewApplication.java
+++ src/main/java/com/nisha/aiintervi/AiinterviewApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 	    public static void main(String[] args) {
 	        SpringApplication.run(AiinterviewApplication.class, args);
+	        System.err.println("Server started successfuully....!");
 	    }
 	}
 
